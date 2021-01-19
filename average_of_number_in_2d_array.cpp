ea7d184b6e211b9f32562a305c029e2ae59c3492
EC2 Default User <ec2-user@ip-172-31-84-131.ec2.internal>
2021-01-19 18:38:56 +0000

    average of numbers in 2d array

---
+++ average_of_number_in_2d_array.cpp
@@ -0,0 +1,57 @@
+// Copyright (C) 2002 Donggeun Lim All rights reserved.
+//
+// Created by: Donggeun Lim
+// Created on: Jan 2021
+// This program calculates average of number in 2d array
+
+
+#include <iostream>
+#include <time.h>
+
+
+template <int rows, int cols>
+float calculateAverage(int (&passedIn2DList)[rows][cols]) {
+    // this function calculates the average
+
+    int total = 0;
+    float average = 0;
+
+    for (size_t rowElement = 0; rowElement < rows; ++rowElement) {
+        for (size_t columnElement = 0; columnElement < cols; ++columnElement)
+            total += passedIn2DList[rowElement][columnElement];
+    }
+    int row = (sizeof(passedIn2DList) / sizeof(passedIn2DList[0]));
+    int col = sizeof passedIn2DList[0] / sizeof(int);
+    average = total / (row * col * 1.0);
+
+    return average;
+}
+
+
+int main() {
+    // this function shows the average of numbers in 2d array
+
+    int aSingleRandomNumber = 0;
+    float average = 0;
+    const int rows = 2;
+    const int columns = 3;
+    int a2DArray[rows][columns];
+
+    srand(time(NULL));
+
+    // input
+    for (int rowElement = 0; rowElement < rows; rowElement++) {
+        for (int columnElement = 0; columnElement < columns; columnElement++) {
+            aSingleRandomNumber = (rand() % 50) + 1;
+            a2DArray[rowElement][columnElement] = aSingleRandomNumber;
+            std::cout << aSingleRandomNumber << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    average = calculateAverage(a2DArray);
+
+    std::cout.precision(4);
+    std::cout << "The average of all numbers is: " << average
+              << std::endl;
+}
